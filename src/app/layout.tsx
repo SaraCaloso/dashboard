0d1d009b604f256bdf7cc8636a4ae6f0d3e5150c
SaraCaloso <sarahi.caloso@g-tech.io>
2025-08-08 13:56:46 -0600

    sepa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import {
   SidebarProvider,
 } from "@/components/ui/sidebar"
 import { Footer } from "@/components/layout/footer";
-import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ConvexClientProvider } from "../../convex/ConvexClientProvider";
 
 export const iframeHeight = "800px"
 
